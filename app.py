4c8a70d5c6c94fe4174c154e55fa58799a19904d
Chinmay Hegde <chinmayhegde001@gmail.com>
2026-04-11 20:37:45 +0530

    fix: added port binding for render deployment

--- app.py
+++ app.py
@@ -365,4 +365,5 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
